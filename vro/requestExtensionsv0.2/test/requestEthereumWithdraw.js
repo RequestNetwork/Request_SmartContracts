d73a2e3f4914114104cd7ad29c25b3cbff1b7cd0
Vincent Rolland <vincent.rolland.38@gmail.com>
2017-08-28 11:42:57 +0200

    introduce TEST withdraw

---
+++ vro/requestExtensionsv0.2/test/requestEthereumWithdraw.js
@@ -0,0 +1,161 @@
+
+var RequestCore = artifacts.require("./RequestCore.sol");
+var RequestEthereum = artifacts.require("./RequestEthereum.sol");
+// contract for test
+var TestRequestReentrance = artifacts.require("./TestRequestReentrance.sol");
+var BigNumber = require('bignumber.js');
+
+var SolidityCoder = require("web3/lib/solidity/coder.js");
+
+
+var getEventFromReceipt = function(log, abi) {
+	var event = null;
+
+	for (var i = 0; i < abi.length; i++) {
+	  var item = abi[i];
+	  if (item.type != "event") continue;
+	  var signature = item.name + "(" + item.inputs.map(function(input) {return input.type;}).join(",") + ")";
+	  var hash = web3.sha3(signature);
+	  if (hash == log.topics[0]) {
+	    event = item;
+	    break;
+	  }
+	}
+
+	if (event != null) {
+	  var inputs = event.inputs.map(function(input) {return input.type;});
+	  var data = SolidityCoder.decodeParams(inputs, log.data.replace("0x", ""));
+	  // Do something with the data. Depends on the log and what you're using the data for.
+	  return {name:event.name , data:data};
+	}
+	return null;
+}
+
+var expectThrow = async function(promise) {
+  try {
+    await promise;
+  } catch (error) {
+    // TODO: Check jump destination to destinguish between a throw
+    //       and an actual invalid jump.
+    const invalidOpcode = error.message.search('invalid opcode') >= 0;
+    const invalidJump = error.message.search('invalid JUMP') >= 0;
+    // TODO: When we contract A calls contract B, and B throws, instead
+    //       of an 'invalid jump', we get an 'out of gas' error. How do
+    //       we distinguish this from an actual out of gas event? (The
+    //       testrpc log actually show an 'invalid jump' event.)
+    const outOfGas = error.message.search('out of gas') >= 0;
+    assert(
+      invalidOpcode || invalidJump || outOfGas,
+      "Expected throw, got '" + error + "' instead",
+    );
+    return;
+  }
+  assert.fail('Expected throw not received');
+};
+
+
+contract('RequestEthereum Withdraw',  function(accounts) {
+	var admin = accounts[0];
+	var hacker = accounts[1];
+	var fakeContract = accounts[2];
+	var payer = accounts[3];
+	var payee = accounts[4];
+
+	var requestCore;
+	var requestEthereum;
+	var newRequest;
+
+	var arbitraryAmount = 100000000000000000;
+	var arbitraryAmount10percent = 10000000000000000;
+	var testRequestReentrance;
+
+    beforeEach(async () => {
+		requestCore = await RequestCore.new({from:admin});
+    	requestEthereum = await RequestEthereum.new(requestCore.address,{from:admin});
+
+		await requestCore.adminResume({from:admin});
+		await requestCore.adminAddTrustedSubContract(requestEthereum.address, {from:admin});
+
+		var newRequest = await requestEthereum.createRequest(payee, payer, arbitraryAmount, [], [], [], [], {from:payee});
+		await requestEthereum.accept(1, {from:payer});
+    });
+
+	// ##################################################################################################
+	// ### Accept test unit #############################################################################
+	// ##################################################################################################
+
+	it("withdraw when amount[msg.sender] = 0 & contract value = 0 Impossible", async function () {
+		// await requestEthereum.pay(1, {from:payer,value:arbitraryAmount});
+		assert.equal(await requestEthereum.ethToWithdraw.call(payee),0,"Balance of payee must be 0" );
+		await expectThrow(requestEthereum.withdraw());
+	});
+
+	it("withdraw when amount[msg.sender] = 0 & contract value != 0 Impossible", async function () {
+		await requestEthereum.pay(1, {from:payer,value:arbitraryAmount});
+		assert.equal(await requestEthereum.ethToWithdraw.call(payer),0,"Balance of payer must be 0" );
+		await expectThrow(requestEthereum.withdraw());
+	});
+
+	it("withdraw when amount[msg.sender] = X, msg.sender get X and amount[msg.sender] = 0", async function () {
+		var balancePayee = Math.floor(await web3.eth.getBalance(payee) / arbitraryAmount);
+		await requestEthereum.pay(1, {from:payer,value:arbitraryAmount});
+		assert.equal(await requestEthereum.ethToWithdraw.call(payee),arbitraryAmount,"Balance of payee must be arbitraryAmount" );
+		await requestEthereum.withdraw({from:payee});
+		assert.equal(await requestEthereum.ethToWithdraw.call(payee),0,"Balance of payee must be 0" );
+		var newBalance = Math.floor(await web3.eth.getBalance(payee) / arbitraryAmount);
+
+		assert.equal(newBalance ,balancePayee+1,"Payee must be credited by arbitraryAmount");
+	});
+
+
+	// TODO : 	// OP CODE not understood /!\
+	// it("challenge reentrance 2 rounds", async function () {
+	//	await requestEthereum.pay(1, {from:payer,value:arbitraryAmount});
+		
+	// 	console.log("await requestEthereum.ethToWithdraw.call(payee)");
+	// 	console.log(await requestEthereum.ethToWithdraw.call(payee));
+
+	// 	testRequestReentrance = await TestRequestReentrance.new(requestEthereum.address, 2,{from:hacker});
+	// 	var r = await testRequestReentrance.init(hacker,{from:hacker});
+
+	// 	assert.equal(r.logs[0].event,"Log","Event Log is missing");
+	// 	assert.equal(r.logs[0].args.id,2,"Event LogRequestPayment wrong args id");
+
+	// 	await requestEthereum.accept(r.logs[0].args.id, {from:hacker});
+	// 	await requestEthereum.pay(2, {from:hacker,value:arbitraryAmount10percent});
+
+
+	// 	console.log("await requestEthereum.ethToWithdraw.call(testRequestReentrance.address)");
+	// 	console.log(await requestEthereum.ethToWithdraw.call(testRequestReentrance.address));
+
+	// 	console.log("await web3.eth.getBalance(hacker)");
+	// 	console.log(await web3.eth.getBalance(hacker));
+
+	// 	console.log("await web3.eth.getBalance(requestEthereum.address)");
+	// 	console.log(await web3.eth.getBalance(requestEthereum.address));
+
+	// 	console.log("await web3.eth.getBalance(testRequestReentrance.address)");
+	// 	console.log(await web3.eth.getBalance(testRequestReentrance.address));
+
+	// 	var r = await testRequestReentrance.start({from:hacker});
+	// 	// web3.eth.sendTransaction({from:hacker, to:testRequestReentrance.address, value: 1});
+	// 	console.log("############################################################################");
+	// 	console.log("############################################################################");
+	// 	console.log("############################################################################");
+
+	// 	console.log("await requestEthereum.ethToWithdraw.call(testRequestReentrance.address)");
+	// 	console.log(await requestEthereum.ethToWithdraw.call(testRequestReentrance.address));
+
+	// 	console.log("await web3.eth.getBalance(hacker)");
+	// 	console.log(await web3.eth.getBalance(hacker));
+
+	// 	console.log("await web3.eth.getBalance(requestEthereum.address)");
+	// 	console.log(await web3.eth.getBalance(requestEthereum.address));
+
+	// 	console.log("await web3.eth.getBalance(testRequestReentrance.address)");
+	// 	console.log(await web3.eth.getBalance(testRequestReentrance.address));
+	// });
+
+
+});
+
