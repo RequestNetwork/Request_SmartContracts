7951a728b460427dfd06fe8f5e3f076ae14048a8
vrolland <vincent.rolland.38@gmail.com>
2017-10-31 15:08:04 +0100

    compiler 0.4.15

--- contracts/test/syncrhone/TestRequestReentrance.sol
+++ contracts/test/syncrhone/TestRequestReentrance.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.4.11;
-
+pragma solidity 0.4.15;
 
 contract RequestEthereumWeak {
 
--- contracts/test/syncrhone/TestRequestSynchroneSubContractLauncher.sol
+++ contracts/test/syncrhone/TestRequestSynchroneSubContractLauncher.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.15;
 
 import '../../core/RequestCore.sol';
 import '../../synchrone/extensions/RequestSynchroneInterface.sol';
